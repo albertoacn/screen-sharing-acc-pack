38bf428f0fe1a4113fa1e3532fd0b07822294136
acn <acn@MacBookCapitanUK.local>
2016-09-25 22:29:15 +0200

    Removing API config info

--- android/ScreensharingAccPackKit/screensharing-acc-pack-kit/src/androidTest/java/com/tokbox/android/accpack/screensharing/config/APIConfig.java
+++ android/ScreensharingAccPackKit/screensharing-acc-pack-kit/src/androidTest/java/com/tokbox/android/accpack/screensharing/config/APIConfig.java
@@ -4,10 +4,10 @@ import com.tokbox.android.accpack.screensharing.utils.TestUtils;
 
 public class APIConfig {
 
-    public static int API_KEY = 100; // Fill this in with generated API Key (http://tokbox.com/opentok/api/tools/js/apikey)
+    public static int API_KEY = 0; // Fill this in with generated API Key (http://tokbox.com/opentok/api/tools/js/apikey)
 
     public static String API_SECRET = ""; // Fill this in with generated API Secret
 
     public static String TOKEN = "";
-    public static String SESSION_ID = "123456789";
+    public static String SESSION_ID = "";
 }
