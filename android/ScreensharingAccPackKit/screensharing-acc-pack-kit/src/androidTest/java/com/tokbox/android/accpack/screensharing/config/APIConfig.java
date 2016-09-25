58280923f98fdc67dd8b744688d0e659a2d6ba52
acn <acn@MacBookCapitanUK.local>
2016-09-25 22:26:04 +0200

    Adding unit/instrumentation tests

---
+++ android/ScreensharingAccPackKit/screensharing-acc-pack-kit/src/androidTest/java/com/tokbox/android/accpack/screensharing/config/APIConfig.java
@@ -0,0 +1,13 @@
+package com.tokbox.android.accpack.screensharing.config;
+
+import com.tokbox.android.accpack.screensharing.utils.TestUtils;
+
+public class APIConfig {
+
+    public static int API_KEY = 100; // Fill this in with generated API Key (http://tokbox.com/opentok/api/tools/js/apikey)
+
+    public static String API_SECRET = ""; // Fill this in with generated API Secret
+
+    public static String TOKEN = "";
+    public static String SESSION_ID = "123456789";
+}
