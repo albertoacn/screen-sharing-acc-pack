21030a1ecd6e45f7e3a4b2d682fd53a751fafe06
acn <acn@MacBookCapitanUK-3.local>
2016-10-10 19:17:53 +0200

    Removing sessionID/token generator

--- android/ScreensharingAccPackKit/screensharing-acc-pack-kit/src/androidTest/java/com/tokbox/android/accpack/screensharing/config/constants/RoleConstants.java
+++
@@ -1,8 +0,0 @@
-package com.tokbox.android.accpack.screensharing.config.constants;
-
-
-public class RoleConstants {
-    public static final String SUBSCRIBER = "subscriber"; //Can only subscribe
-    public static final String PUBLISHER = "publisher";   //Can publish, subscribe, and signal
-    public static final String MODERATOR = "moderator";   //Can do the above along with  forceDisconnect and forceUnpublish
-}
--- android/ScreensharingAccPackKit/screensharing-acc-pack-kit/src/androidTest/java/com/tokbox/android/accpack/screensharing/testbase/TestRunner.java
+++ android/ScreensharingAccPackKit/screensharing-acc-pack-kit/src/androidTest/java/com/tokbox/android/accpack/screensharing/testbase/TestRunner.java
@@ -4,11 +4,8 @@ package com.tokbox.android.accpack.screensharing.testbase;
 import android.os.Bundle;
 
 import com.tokbox.android.accpack.screensharing.config.APIConfig;
-import com.tokbox.android.accpack.screensharing.config.OpenTokSDK;
-import com.tokbox.android.accpack.screensharing.config.OpenTokSession;
 import com.zutubi.android.junitreport.JUnitReportTestRunner;
 
-import java.util.HashMap;
 
 
 public class TestRunner extends JUnitReportTestRunner {
@@ -26,20 +23,6 @@ public class TestRunner extends JUnitReportTestRunner {
         APIConfig.API_URL = arguments.getString(FLAG_API_URL, APIConfig.API_URL);
         APIConfig.SESSION_ID = arguments.getString(FLAG_API_SESSION_ID, APIConfig.SESSION_ID);
         APIConfig.TOKEN = arguments.getString(FLAG_API_TOKEN, APIConfig.TOKEN);
-
-        try {
-            OpenTokSDK openTokSDK = null;
-            if (APIConfig.SESSION_ID == "") {
-                openTokSDK = new OpenTokSDK(Integer.parseInt(APIConfig.API_KEY), APIConfig.API_SECRET);
-                APIConfig.SESSION_ID = openTokSDK.create_session("127.0.0.1", new HashMap<String, String>()).getSessionId();
-            }
-            if (APIConfig.TOKEN == "") {
-                openTokSDK = openTokSDK != null ? openTokSDK : new OpenTokSDK(Integer.parseInt(APIConfig.API_KEY), APIConfig.API_SECRET);
-                APIConfig.TOKEN = openTokSDK.generate_token(APIConfig.SESSION_ID);
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
 
         super.onCreate(arguments);
     }
