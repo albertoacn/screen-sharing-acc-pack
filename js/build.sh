617fcd0671cd34ce312cb36c41cbd08eaa1b6a9e
acn <acn@MacBookCapitanUK-3.local>
2016-11-16 21:25:46 +0100

    Adding unit tests for JS

--- js/build.sh
+++ js/build.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 set -e
+task="$1"
 
 if [[ -d opentok.js-screen-sharing ]]
 then
 	cd opentok.js-screen-sharing
 	gulp dist
 	gulp zip
-  cd dist
 
 else
 	echo "Please run this script from 'JS'."
 	exit 1
+fi
+
+if [ "$task" == "-t" ]; then
+        grunt karma:prod
 fi
