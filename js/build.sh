21162d301f201a98d2ca0b786f89fb846f1beeb3
acn <acn@MacBookCapitanUK-3.local>
2017-01-05 12:34:46 +0100

    Adding force options for test script JS

--- js/build.sh
+++ js/build.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 set -e
 task="$1"
+force="$2"
 
-if [[ -d opentok.js-screen-sharing ]]
-then
-	cd opentok.js-screen-sharing
-	gulp dist
-	gulp zip
-
-else
-	echo "Please run this script from 'JS'."
-	exit 1
+if [ "$task" == "-d" ]; then
+	if [[ -d opentok.js-screen-sharing ]]
+	then
+		cd opentok.js-screen-sharing
+		gulp dist
+		gulp zip
+	else
+		echo "Please run this script from 'JS'."
+		exit 1
+	fi
 fi
 
 if [ "$task" == "-t" ]; then
-        grunt karma:prod
+	cd opentok.js-screen-sharing
+	grunt karma:prod "$force"
+	exit 0
 fi
